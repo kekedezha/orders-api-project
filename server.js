8909dee8e2ac4b54ada89ff04b9e8542d0c8bba1
kekedezha <dezhac6@gmail.com>
2025-02-10 18:53:34 -0500

    add PUT route to update orders.json file based on matching id

--- server.js
+++ server.js
@@ -15,13 +15,36 @@ server.get("/orders", (request, response) => {
 });
 
 //Add the /neworder code here!
+//use express.json() middleware to parse an incoming POST request with new order data
 server.post("/neworder", express.json(), (request, response) => {
   orderData.orders.push(request.body);
   fs.writeFileSync("orders.json", JSON.stringify(orderData));
   response.send("Successful request. Created new order.");
   console.log("Successful request. Created new order");
 });
+
 //Add the /update/:id code here!
+//use express.text() middleware to parse
+server.put(
+  "/update/:id",
+  express.text({ type: "*/*" }),
+  (request, response) => {
+    var items = orderData.orders;
+
+    items.forEach(function (o) {
+      console.log(o);
+      if (o.id == request.params.id) {
+        console.log("Modifying order!");
+        o.state = request.body;
+      }
+    });
+
+    fs.writeFileSync("orders.json", JSON.stringify(orderData));
+
+    response.send("Successful order update for matching id.");
+    console.log("Successful order update for matching id.");
+  }
+);
 
 //Add the /delete/:id code here!
 
